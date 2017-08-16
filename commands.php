43f66812492a34d7036beed7e458317b0e054624
eohomegrownapps <euan.l.y.ong@gmail.com>
2017-08-16 23:52:54 +0800

    Moved json data into separate file

--- commands.php
+++ commands.php
@@ -1,23 +1,6 @@
 <?php
 //Change this
-$user = '{
-	"jbloggs": {
-		"password": "Password1",
-		"greeting": "Welcome, Joe Bloggs. Your security clearance level is 1.",
-		"files": {
-			"MyTest.txt": "boehgowajopwejaofpnwongpw",
-			"OtherFile.txt": "Lorem ipsum dolor sit amet",
-			"secret_password": "The password to jdoe\u0027s account is <br>123456789"
-		}
-	},
-	"jdoe": {
-		"password": "123456789",
-		"greeting": "Welcome, John Doe. Your security clearance level is 2.",
-		"files": {
-			"blah.txt": "123456789<br>12345678<br>234567<br>2345678"
-		}
-	}
-}';
+require 'gamedata.php';
 
 $users = json_decode($user);
 
---
+++ gamedata.php
@@ -0,0 +1,20 @@
+<?php
+$user = '{
+	"jbloggs": {
+		"password": "Password1",
+		"greeting": "Welcome, Joe Bloggs. Your security clearance level is 1.",
+		"files": {
+			"MyTest.txt": "boehgowajopwejaofpnwongpw",
+			"OtherFile.txt": "Lorem ipsum dolor sit amet",
+			"secret_password": "The password to jdoe\u0027s account is <br>123456789"
+		}
+	},
+	"jdoe": {
+		"password": "123456789",
+		"greeting": "Welcome, John Doe. Your security clearance level is 2.",
+		"files": {
+			"blah.txt": "123456789<br>12345678<br>234567<br>2345678"
+		}
+	}
+}';
+?>
